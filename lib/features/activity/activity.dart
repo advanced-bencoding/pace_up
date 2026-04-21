868554cfc8fd0a1e4995ae1bd5479e603fabbb0b
ben <benjamin.joseph212620@gmail.com>
2026-04-21 21:23:05 +0530

    Add location permissions and geolocator plugin support for multiple platforms

--- lib/features/activity/activity.dart
+++ lib/features/activity/activity.dart
@@ -5,7 +5,6 @@ class Activity extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return const Text("Activity View");
-    }
+  }
 }
